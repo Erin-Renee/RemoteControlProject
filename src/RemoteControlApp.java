86f332918ca6f8f7b2bdc6f5d239f220a1a90661
Erin Eustice <erinrenee.va@gmail.com>
2019-11-08 17:27:26 -0700

    Added while loop, Initial prompt, pressButton() method

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
 
 public class RemoteControlApp {
-
+	/*
+	 * Overview 
+	 * We are going to simulate a remote control. It will have buttons "0"
+	 * to "9", "ON", and "OFF". In order to operate the remote control, the user
+	 * must first turn it on. Once it is turned on, the user can enter a number from
+	 * 0 through 9, or turn the remote control back off.
+	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("To Begin, Turn ON The Remote Control: ");
+		String userInput = keyboard.next();
+		
+		while (!userInput.equals("ON")) {
+			System.out.println("Please Turn ON Remote To Use Features");
+			userInput = keyboard.next();
+			
+		}
+		pressButton();
+		
+	}
+	
+	public static void pressButton() {
+		System.out.println("button Pressed");
 	}
 // TODO: Add pressButton() method
 // TODO: AddturnOn() method
